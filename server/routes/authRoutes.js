91d0a0a92357f45ab682edee90bdc75490338b61
NzamaE <nzamaernest1@gmail.com>
2025-09-10 19:01:12 +0200

    Update

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -9,23 +9,24 @@ const router = express.Router();
 //Register
 router.post("/register", async (req, res) => {
 
+    const theDb = await connectToDatabase();
 
-  const theDb = await connectToDatabase();
+    const user = {
+        name: req.body.name,
+        surname: req.body.surname,
+        email: req.body.email,
+        role: req.body.role,
+        password: req.body.password
+    }
 
-const user ={
-name : req.body.name,
-surname : req.body,
-email : req.body.email,
-role : req.body.role,
-password : req.body.password 
-}
+    const response = await theDb.collection('Users').insertOne(user);
+
+    console.log("Database insertion response:" + response);
 
 
-  //  console.log(JSON.parse(req.body.name));
+    //  console.log(JSON.parse(req.body.name));
     console.log(JSON.stringify(user));     // e.g. "John"
-    
 
-    
     res.status(200).json({
         message: "User registered successfully",
         data: req.body
